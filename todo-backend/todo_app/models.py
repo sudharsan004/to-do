14a171695b7a5682934f6ecdc1270d970dd4d2e2
sudharsan004 <sudharsansriram8921@gmail.com>
2021-05-16 08:32:02 +0530

    add user as foreign key to todo model

--- todo-backend/todo_app/models.py
+++ todo-backend/todo_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -10,6 +10,7 @@ class Category(models.Model):
         return self.name
 
 class Todo(models.Model):
+    user=models.ForeignKey(User, on_delete= models.CASCADE)
     task = models.CharField(max_length=500)
     category = models.ForeignKey(Category,on_delete=CASCADE)
     #schedule_time = models.DateTimeField(auto_now_add=True,null=True)
